520f9201ee5a6d681e4643c1b82abd30d000536e
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-03-17 16:33:10 +0530

    comments

---
+++ Chapter_02/comments.py
@@ -0,0 +1,21 @@
+print("abcd1")
+# print("abcd2")
+
+# comment rule 1: use # to comment any line,single line comment
+
+# print("kfugvywfisdyufuwfguw")
+
+# comment rule 2: """  """ to make multi line comment
+
+"""
+print("hello world avv")
+print("hello world avv")
+print("hello world avv")
+print("hello world avv")
+print("hello world avv")
+print("hello world avv")
+print("hello world avv")
+print("hello world avv")
+print("hello world avv")
+print("hello world avv")
+"""
