b157a5d6e8e33f0bc24a7d4be1f29808de2a9b91
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-03-17 16:33:26 +0530

    syntax

---
+++ Chapter_02/syntax.py
@@ -0,0 +1,22 @@
+# print("we are going to learn syntax in python")
+
+# syntax rule 1: use ""/''/" ' " : valid cases
+
+# valid  cases
+# print("hello world")
+# print('hello world')
+# print("hello world's ")
+
+# syntax rule 2: never use : """/'''
+
+# invalid cases
+# print("what"s up")
+# print('waht's up')
+
+# syntax rule 3: use tab after if else
+age = 20
+if (age > 18):
+    print('vote eligible')
+    print('good')
+else:
+    print("vote not eligible")
