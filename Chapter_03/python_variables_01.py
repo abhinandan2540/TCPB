9944988de83c2eb5a8a9181c8188f535b3fbd83c
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-04-01 12:28:12 +0530

    variables-01

---
+++ Chapter_03/python_variables_01.py
@@ -0,0 +1,30 @@
+# x = 'python language'
+# y = 10
+# z = 9.8
+
+# print(x)
+# print(y)
+# print(z)
+
+# typeof()
+
+# x = 'manaiii'
+# y = 10
+# z = 3.17
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# 3.0 - 3
+
+x = 3
+y = 3.7
+
+x1 = str(x)
+y1 = int(y)
+
+print(x1)
+print(y1)
+print(type(x1))
+print(type(y1))
