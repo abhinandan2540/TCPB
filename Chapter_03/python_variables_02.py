8f214d744edea17e0f525507288338cb74d8bcad
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-04-08 15:58:15 +0530

    variable naming rules

---
+++ Chapter_03/python_variables_02.py
@@ -0,0 +1,61 @@
+
+# x = 3
+# y = 'avv'
+
+# print(x)
+# print(y)
+
+# rule 1 : python variable case sensitive
+# age = 20
+# Age = 20
+# AGE = 20
+
+# print(age)
+# print(Age)
+# print(AGE)
+
+# rule 2: variable naming should start with words not with numebrs
+# My2Name = "avv"
+# 2MyName = 'avv'
+# print(My2Name)
+# print(2MyName)
+
+# rule 3: in variable naming, only we can use A-z,0-9,_
+
+# my2name = 'avv'
+# my_name = 'avv'
+# # my-name = 'avv'
+# # my & name = 'avv'
+
+# print(my2name)
+# print(my_name)
+# # print(my-name)
+# # print(my & name)
+
+
+# rule 4: don't use python keyword for creating variable name
+
+# name = 'avv'
+# # while = 'avv'
+
+# print(name)
+# # print(while)
+
+
+# variable naming culture:
+# 1. pascal case naming
+# 2. camel case naming
+# 3. snake case naming
+
+
+# 1. pascal case :
+MyVariableName = 'pascal'
+print(MyVariableName)
+
+# 2. camelcase :
+myVariableName = "camel"
+print(myVariableName)
+
+# 3. snake case:
+my_variable_name = 'snake'
+print(my_variable_name)
